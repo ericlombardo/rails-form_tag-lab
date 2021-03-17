fbf2c93cd980f4b903d2d0a0587f6b86944216a1
Eric <eric.m.lombardo@gmail.com>
2021-03-17 15:08:54 -0400

    Use form_tag and helpers to create for and submit

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,4 +8,12 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  def new
+  end
+
+  def create  # redirect back to new view with params showing
+    @params = params
+    render :new
+  end
+
 end
